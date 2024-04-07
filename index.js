bea60f5c57a5b1e44a278b9e2b0b05c69a4325b0
Sabrinagh24 <sabrina.tachefine@fgei.ummto.dz>
2024-04-07 16:01:38 +0100

    modif

---
+++ index.js
@@ -0,0 +1,10 @@
+  var slider = tns({
+    container: '.slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: true,
+    mouseDrag: true,
+    autoplayButtonOutput: false,
+    controls: false,
+    nav: false,
+  });
