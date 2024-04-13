aa6167dca6092b53545e6582babf414774ce1760
Sabrinagh24 <sabrina.tachefine@fgei.ummto.dz>
2024-04-13 19:17:57 +0100

    mods

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-  var slider = tns({
+var slider = tns({
     container: '.slider',
     items: 1,
     slideBy: 'page',
     autoplay: true,
-    mouseDrag: true,
+    mouseDrag: false,
     autoplayButtonOutput: false,
     controls: false,
-    nav: false,
+    navPosition : "bottom",
+    nav: true
+
+  });
+var slider = tns({
+    container: '.slider2',
+    items: 2,
+    slideBy: 'page',
+    autoplay: false,
+    mouseDrag: false,
+    autoplayButtonOutput: true,
+    controls: true,
+    nav: true,
+    prevButton : ".previous",
+    nextButton : ".next",
+    navPosition : "bottom"
   });
